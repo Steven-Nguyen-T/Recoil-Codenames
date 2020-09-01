fd577be89759426c5d55181b09dac24b6d036236
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-01 13:50:28 -0700

    update containers

--- src/components/TeamSide/TeamSideContainer.js
+++ src/components/TeamSide/TeamSideContainer.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 const TeamSideContainer = () => {
-  return <div>this is a team side container</div>;
+  const classes = useStyles();
+
+  return (
+    <List component="nav" className={classes.root} aria-label="mailbox folders">
+      <ListItem button>Score:</ListItem>
+      <Divider />
+      <ListItem button divider>
+        Operatives
+      </ListItem>
+      <ListItem button>Spymaster</ListItem>
+      <Divider light />
+    </List>
+  );
 };
 
 export default TeamSideContainer;
