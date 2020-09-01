ddf726cf9b9b8c715b781c2c34e3d8a1b637ff8e
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-01 12:00:53 -0700

    grid form update

--- src/components/GameBoard/gameBoardContainer.js
+++ src/components/GameBoard/gameBoardContainer.js
@@ -1,8 +1,81 @@
 // Function component, should reder 25 gameTiles, one team with 9, on team with 8
-import React from "react";
+import React, { useState } from "react";
 
-const GameBoardContainer = () => {
-  return <div>This is the gameBoard Container</div>;
-};
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-export default GameBoardContainer;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function GameBoardContainer() {
+  const classes = useStyles();
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  const [gameBoard, setGameBoard] = useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+  ]);
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
---
+++ src/components/GameBoard/tileData.js
@@ -0,0 +1,12 @@
+const tileData = [
+  {
+    img: null,
+    title: "tile 1",
+    author: "author 1",
+  },
+  {
+    img: null,
+    title: "tile 2",
+    author: "author 2",
+  },
+];
