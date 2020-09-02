94ce01627426dbadde1f060f91f1922bec5dcd8d
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-01 18:51:24 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, atom, selector } from "recoil";
+import RecoilizeDebugger from "recoilize";
+
+const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "dsdff", // default value (aka initial value)
+});
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  },
+});
 
 ReactDOM.render(
   <RecoilRoot>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <RecoilizeDebugger
+      nodes={[textState, charCountState]}
+      root={document.getElementById("root")}
+    />
+    <App />
   </RecoilRoot>,
   document.getElementById("root")
 );
