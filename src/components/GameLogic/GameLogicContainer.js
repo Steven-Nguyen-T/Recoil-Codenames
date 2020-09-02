e8e5ea0e0066c84238a6d9e6580753f6ed4e1efe
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-02 10:27:00 -0700

    updating the game logi

--- src/components/GameLogic/GameLogicContainer.js
+++ src/components/GameLogic/GameLogicContainer.js
@@ -1,10 +1,18 @@
 // Typing the word and hitting enter stuff
 import React from "react";
 
+import * as atoms from "../../recoil/store";
+import { useRecoilState } from "recoil";
+
+const { gameBoardState } = atoms;
 // subscribe to the game board and the team sides, when hit will reset
 
 const GameLogicContainer = () => {
-  return <button>next game</button>;
+  const [gameBoard, setGameBoard] = useRecoilState(gameBoardState);
+
+  // create function to create a new game board
+
+  return <button onClick={() => setGameBoard(["new"])}>next game</button>;
 };
 
 export default GameLogicContainer;
